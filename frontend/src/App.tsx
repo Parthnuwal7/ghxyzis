c5d8e2ecfd368cde701ea23ef6a420552f5d658e
Parth Nuwal <156829642+Parthnuwal7@users.noreply.github.com>
2025-07-23 02:12:58 +0530

    Add files via upload

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LayoutBuilder from './components/LayoutBuilder';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LayoutBuilder />
+    </div>
+  );
+}
+
+export default App;
